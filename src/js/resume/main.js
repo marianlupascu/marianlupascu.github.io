d676e69fc5fa5a45b41875eead9cf8562ae6d68b
Lupascu Marian <marianlupascu15@gmail.com>
2018-10-02 22:48:46 +0300

    Load Resume :crescent_moon:

---
+++ src/js/resume/main.js
@@ -0,0 +1,79 @@
+/* ===================================================================
+ * Count - Main JS
+ *
+ * ------------------------------------------------------------------- */
+
+(function ($) {
+
+  "use strict";
+
+  var cfg = {
+      scrollDuration: 800, // smoothscroll duration
+      mailChimpURL: 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
+    },
+
+    $WIN = $(window);
+
+  // Add the User Agent to the <html>
+  // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
+  var doc = document.documentElement;
+  doc.setAttribute('data-useragent', navigator.userAgent);
+
+  // svg fallback
+  if (!Modernizr.svg) {
+    $(".home-logo img").attr("src", "images/logo.png");
+  }
+
+
+  /* Preloader
+   * -------------------------------------------------- */
+  var ssPreloader = function () {
+
+    $("html").addClass('ss-preload');
+
+    $WIN.on('load', function () {
+
+      // will first fade out the loading animation 
+      $("#loader").fadeOut("slow", function () {
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
+      });
+
+      // for hero content animations 
+      $("html").removeClass('ss-preload');
+      $("html").addClass('ss-loaded');
+
+    });
+  };
+
+
+  /* slick slider
+   * ------------------------------------------------------ */
+  var ssSlickSlider = function () {
+
+    $('.home-slider').slick({
+      arrows: false,
+      dots: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      fade: true,
+      speed: 3000
+    });
+
+  };
+
+  /* initialize
+   * ------------------------------------------------------ */
+  (function ssInit() {
+
+    ssPreloader();
+    ssInfoToggle();
+    ssSlickSlider();
+    ssPlaceholder();
+    ssFinalCountdown();
+    ssAjaxChimp();
+
+  })();
+
+
+})(jQuery);
