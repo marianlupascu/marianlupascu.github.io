90bbb0b845addf1448c48339e75683b5964848f5
Lupascu Marian <marianlupascu15@gmail.com>
2019-02-15 11:34:40 +0200

    Update ComingSoon Page :earth_asia:

--- src/js/comingSoon/main.js
+++ src/js/comingSoon/main.js
@@ -89,7 +89,7 @@
    * ------------------------------------------------------ */
   var ssFinalCountdown = function () {
 
-    var finalDate = '2019/01/30';
+    var finalDate = '2019/08/30';
 
     $('.home-content__clock').countdown(finalDate)
       .on('update.countdown finish.countdown', function (event) {
