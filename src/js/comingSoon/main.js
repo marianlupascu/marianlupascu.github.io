c94f6075a10de558d10d1a5027d23ba703e549dc
Lupascu Marian <marianlupascu15@gmail.com>
2020-02-06 10:55:51 +0200

    Edit deadline :diamonds:

--- src/js/comingSoon/main.js
+++ src/js/comingSoon/main.js
@@ -89,7 +89,7 @@
    * ------------------------------------------------------ */
   var ssFinalCountdown = function () {
 
-    var finalDate = '2020/03/30';
+    var finalDate = '2020/08/30';
 
     $('.home-content__clock').countdown(finalDate)
       .on('update.countdown finish.countdown', function (event) {
